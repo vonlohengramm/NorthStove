f3f43941d3102bb87755fbcec2237be60361a5fb
vonlohengramm <402743725@qq.com>
2020-01-15 09:52:56 +0800

    menu scene structure

--- js/MenuScene.js
+++ js/MenuScene.js
@@ -1,7 +1,24 @@
 var menu = new Phaser.Scene('menu'); // 第二页，选画面
 menu.preload = function () {
-    this.load.image('back-1', 'png/Scene-1.jpg');
+    this.load.image('menu-back', 'png/menu-back.jpg');
+    this.load.image('menu-title', 'png/menu-title.png');
 }
-menu.preload = function () {
-    this.add.image(SCENE_WIDTH / 2 * GENERAL_SCALE, SCENE_HEIGHT / 2 * GENERAL_SCALE, 'back-1').setScale(GENERAL_SCALE)
+menu.create = function () {
+    this.add.image(SCENE_WIDTH / 2 * GENERAL_SCALE, SCENE_HEIGHT / 2 * GENERAL_SCALE, 'menu-back').setScale(GENERAL_SCALE);
+    var prompt = this.add.image(3445 * GENERAL_SCALE, 3087 * GENERAL_SCALE, 'prompt-back').setScale(GENERAL_SCALE);
+    var title = this.add.image(3445 * GENERAL_SCALE, 3097 * GENERAL_SCALE, 'menu-title').setScale(GENERAL_SCALE);
+
+    var startAlpha = 1;
+    this.tweens.add({
+        targets: title,
+        duration: 1000,
+        alpha: {
+            getStart: () => startAlpha,
+            getEnd: () => 1 - startAlpha,
+        },
+        loop: -1,
+        onLoop: function () {
+            startAlpha = 1 - startAlpha;
+        }
+    })
 }
--- js/PrefaceScene.js
+++ js/PrefaceScene.js
@@ -1,15 +1,15 @@
 var preface = new Phaser.Scene('preface'); // 第一页
 preface.preload = function () {
-    this.load.image('back', 'png/preface-back.jpg');
+    this.load.image('preface-back', 'png/preface-back.jpg');
     this.load.image('logo', 'png/logo.png');
     this.load.image('prompt-back', 'png/preface-prompt-back.png');
-    this.load.image('title', 'png/preface-title.png');
+    this.load.image('preface-title', 'png/preface-title.png');
 }
 preface.create =  function () {
-    this.add.image(SCENE_WIDTH / 2 * GENERAL_SCALE, SCENE_HEIGHT / 2 * GENERAL_SCALE, 'back').setScale(GENERAL_SCALE);
+    this.add.image(SCENE_WIDTH / 2 * GENERAL_SCALE, SCENE_HEIGHT / 2 * GENERAL_SCALE, 'preface-back').setScale(GENERAL_SCALE);
     this.add.image(5176.5 * GENERAL_SCALE, 1235.5 * GENERAL_SCALE, 'logo').setScale(GENERAL_SCALE);
     var prompt = this.add.image(3445 * GENERAL_SCALE, 3087 * GENERAL_SCALE, 'prompt-back').setScale(GENERAL_SCALE);
-    var title = this.add.image(3445 * GENERAL_SCALE, 3097 * GENERAL_SCALE, 'title').setScale(GENERAL_SCALE);
+    var title = this.add.image(3445 * GENERAL_SCALE, 3097 * GENERAL_SCALE, 'preface-title').setScale(GENERAL_SCALE);
 
     var startAlpha = 1;
     this.tweens.add({
