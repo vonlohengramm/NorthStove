89f04daf0bedb736706fc7a1045ded20fb05173b
vonlohengramm <402743725@qq.com>
2020-01-21 17:55:56 +0800

    Pic-2 5pic&5pic title

--- js/MenuScene.js
+++ js/MenuScene.js
@@ -3,11 +3,11 @@ menu.preload = function () {
     this.load.image('menu-back', 'png/menu-back.jpg');
     this.load.image('menu-title', 'png/menu-title.png');
 
-    this.load.image('pic-1', 'png/menu-pic-1.png');
-    this.load.image('pic-2', 'png/menu-pic-2.png');
-    this.load.image('pic-3', 'png/menu-pic-3.png');
-    this.load.image('pic-4', 'png/menu-pic-4.png');
-    this.load.image('pic-5', 'png/menu-pic-5.png');
+    for (let i = 1; i <= 5; i++) {
+        this.load.image('pic-' + i, 'png/menu-pic-' + i + '.png');
+        this.load.image('pic-title-' + i, 'png/menu-pic-title-' + i + '.png');
+    }
+
 }
 menu.create = function () {
     this.add.image(SCENE_WIDTH / 2 * GENERAL_SCALE, SCENE_HEIGHT / 2 * GENERAL_SCALE, 'menu-back').setScale(GENERAL_SCALE);
@@ -19,6 +19,12 @@ menu.create = function () {
     var pic3 = this.add.image(1986 * GENERAL_SCALE, 2602 * GENERAL_SCALE, 'pic-3').setScale(GENERAL_SCALE);
     var pic4 = this.add.image(4422 * GENERAL_SCALE, 2602 * GENERAL_SCALE, 'pic-4').setScale(GENERAL_SCALE);
     var pic5 = this.add.image(5649.5 * GENERAL_SCALE, 2602 * GENERAL_SCALE, 'pic-5').setScale(GENERAL_SCALE);
+
+    var picTitle2 = this.add.image(3308.5 * GENERAL_SCALE, 906.5 * GENERAL_SCALE, 'pic-title-1').setScale(GENERAL_SCALE);
+    var picTitle2 = this.add.image(6323.5 * GENERAL_SCALE, 906.5 * GENERAL_SCALE, 'pic-title-2').setScale(GENERAL_SCALE);
+    var picTitle3 = this.add.image(3308.5 * GENERAL_SCALE, 2468.5 * GENERAL_SCALE, 'pic-title-3').setScale(GENERAL_SCALE);
+    var picTitle4 = this.add.image(5184.5 * GENERAL_SCALE, 2468.5 * GENERAL_SCALE, 'pic-title-4').setScale(GENERAL_SCALE);
+    var picTitle5 = this.add.image(6323.5 * GENERAL_SCALE, 2468.5 * GENERAL_SCALE, 'pic-title-5').setScale(GENERAL_SCALE);
 
     var startAlpha = 1;
     this.tweens.add({
